2a4306bf53c66af2a038db67ad3607fac264b828
AStepanyuk <avstepanyuk@gmail.com>
2017-01-14 16:03:50 +0200

    +opimize JS

--- web/js/main.js
+++ web/js/main.js
@@ -1,37 +1,22 @@
-var btn = document.getElementById("testbtn");
-console.log(btn);
-btn.addEventListener("click", function () {
-    alert("Hi!")
-});
 
-var likeBtns = document.getElementsByClassName('btn-add-like');
-console.log(likeBtns);
+function initAjaxLikes() {
 
-function ajaxLike(url, btn) {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            btn.innerText=this.responseText;
+    var likeBtns = $('.btn-add-like');
+    console.log(likeBtns);
 
-            // document.getElementById("demo").innerHTML = this.responseText;
-        }else{
-            console.log("status" + this.status);
-        }
-    };
-    xhttp.open("POST", url, true);
-    xhttp.send();
-}
-for (var i = 0; i < likeBtns.length; i++) {
-    likeBtns[i].addEventListener("click", function (event) {
+    function ajaxLike(url, btn) {
+        $.post(url, {}, function (response) {
+            btn.innerText = response;
+        });
+    }
+  
+
+    likeBtns.click(function (event) {
         event.preventDefault();
-        var projId = this.getAttribute("data-id");
         var href = this.getAttribute("href");
         ajaxLike(href, this);
-        // alert("Hi!" + href);
 
-        /*
-         * отправить лайк на бекэнд
-         * показать сколько лайков
-         */
-    })
+    });
 }
+
+$(initAjaxLikes);
